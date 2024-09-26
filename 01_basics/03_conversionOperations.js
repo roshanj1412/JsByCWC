5e9facf7cc29d51eae1b823d828bafb09009e034
roshanj1412 <144225527+roshanj1412@users.noreply.github.com>
2024-09-26 12:13:51 +0000

    converesion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+
+let score1="33abc"
+console.log(typeof score1);
+console.log(typeof(score1));
+
+let valueInNumber1 = Number(score1)
+
+console.log(valueInNumber1);
+console.log(typeof valueInNumber1);
+
+
+let score2= null
+console.log(typeof score2);
+console.log(typeof(score2));
+
+let valueInNumber2 = Number(score2)
+
+console.log(valueInNumber2);
+console.log(typeof valueInNumber2);
+
+let score3=undefined
+console.log(typeof score3);
+console.log(typeof(score3));
+
+let valueInNumber3 = Number(score3)
+
+console.log(valueInNumber3);
+console.log(typeof valueInNumber3);
