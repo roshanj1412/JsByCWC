dc0e531de59375ed5d7246d8a23bb1251e433fbf
roshanj1412 <144225527+roshanj1412@users.noreply.github.com>
2024-09-27 08:36:00 +0000

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,35 @@
+//Dates
+
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+//console.log(typeof myDate);
+
+//let myCreateDate = new Date(2024, 0, 23)
+//let myCreateDate = new Date("01-14-2024")
+//let myCreateDate = new Date(2023, 0, 23,5,3)
+let myCreateDate = new Date("01-14-2023")
+
+//console.log(myCreateDate);
+
+//console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+//console.log(myCreateDate.getTime());
+
+
+
+
+
+
+
+
+
+
